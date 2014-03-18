9661652a06a8a05005175ff38836443b74ad6fe6
y.nakayama <y.nakayama@otr-jp.com>
2014-03-18 17:59:24 +0900

    implement filter dirty words

---
+++ Harvester.Tests/HarvesterTest.fs
@@ -0,0 +1,34 @@
+﻿module Harvester.Tests
+
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+module HarvesterTest =
+
+  let format dirty =
+    String.replicate (String.length dirty) "*"
+
+  [<Test>]
+  let ``filter char`` () =
+    "hoge" |> Harvester.harvest format ["h"] |> should equal (Some "*oge")
+
+  [<Test>]
+  let ``not filter`` () =
+    "hoge" |> Harvester.harvest format ["a"] |> should equal (Some "hoge")
+
+  [<Test>]
+  let ``filter chars`` () =
+    "hoge" |> Harvester.harvest format ["h"; "e"] |> should equal (Some "*og*")
+
+  [<Test>]
+  let ``filter sign`` () =
+    "!?.#" |> Harvester.harvest format ["."] |> should equal (Some "!?*#")
+
+  [<Test>]
+  let ``filter string`` () =
+    "hogefugapiyo" |> Harvester.harvest format ["fuga"] |> should equal (Some "hoge****piyo")
+
+  [<Test>]
+  let ``filter strings`` () =
+    "hogefugapiyo" |> Harvester.harvest format ["og"; "api"] |> should equal (Some "h**efug***yo")
---
+++ Harvester/Harvester.fs
@@ -0,0 +1,18 @@
+﻿module Harvester
+
+open ParsecClone.CombinatorBase
+open ParsecClone.StringCombinator
+
+type Dirty = string
+type Turn = string
+
+let private dirtyToTurn cutting words = anyOf matchStr words |>> cutting
+
+let private normal = any
+
+let private parser cutting dirtyWords =
+  many (dirtyToTurn cutting dirtyWords <|> normal) >>= foldStrings
+
+let harvest (format: Dirty -> Turn) dirtyWords input =
+  let state = makeStringStream input
+  state |> parser format dirtyWords |> fst
